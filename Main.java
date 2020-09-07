c4eac92f59f39a0e85c8c9e20cf614e4967b1cdf
Duke Victory <lonespartan12@hotmail.com>
2020-09-07 06:11:03 +0000

    initial commit with code

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+
+    // promp user
+    System.out.println("Enter first name: ");
+    String fName = scan.nextLine();
+    System.out.println("Enter middle name: ");
+    String mName = scan.nextLine();
+    System.out.println("Enter last name");
+    String lName = scan.nextLine();
+
+    //parse first letter from each name
+    char fInitial = fName.charAt(0);
+    char mInitial = mName.charAt(0);
+    char lInitial = lName.charAt(0);
+
+
+    // display users inputs and calculate output
+    System.out.println("Your name is: " + fName + " " + mName + " " + lName + ".");
+    System.out.println("Your initials are: " + fInitial + ". " + mInitial + ". " + lInitial + ".");
+
+    scan.close();
   }
 }
